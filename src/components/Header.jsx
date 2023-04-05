5ffbda24a53d32405252a679b419a3ab46987779
Bryan <bryansanudo95@gmail.com>
2023-04-05 14:38:53 -0500

    deploy gallery

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { MdNightsStay, MdWbSunny, MdOutlineClose } from "react-icons/md";
 import { BiCart } from "react-icons/bi";
 import logo from "@/assets/Logo.png";
+import logoPhoto from "@/assets/LogoPhoto.png";
 
 const Header = ({
   allProducts,
@@ -28,13 +29,25 @@ const Header = ({
     setCountProducts(0);
   };
   return (
-    <header className="fixed w-full bg-gray-900  text-white z-50">
+    <header className="fixed w-full bg-gray-900 text-white z-50">
       <nav className="flex justify-between items-center p-5">
-        <img
-          src={logo}
-          alt=""
-          className="rounded-full object-cover  h-24 w-24  duration-300 shadow-lg   shadow-[#ffcdc2]"
-        />
+        <div className="flex gap-6">
+          <a href="https://eliana-nweed.netlify.app/">
+            <img
+              src={logo}
+              alt=""
+              className="rounded-full object-cover hover:scale-105 transition-300  h-24 w-24  duration-300 shadow-lg   shadow-[#ffcdc2]"
+            />
+          </a>
+          <a href="https://eliana-nweed-gallery.netlify.app/">
+            <img
+              src={logoPhoto}
+              alt=""
+              className="rounded-full object-cover hover:scale-105 transition-300  h-24 w-24  duration-300 shadow-lg   shadow-[#ffcdc2]"
+            />
+          </a>
+        </div>
+
         <div onClick={() => setDarkMode(!darkMode)}>
           {darkMode ? (
             <MdWbSunny className="w-8 h-8 cursor-pointer text-[#ffcdc2]" />
