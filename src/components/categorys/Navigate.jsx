e25aa4fd1e72f193838764a44596a930145d5976
Bryan <bryansanudo95@gmail.com>
2023-04-06 00:43:50 -0500

    navigate pink

--- src/components/categorys/Navigate.jsx
+++ src/components/categorys/Navigate.jsx
@@ -9,7 +9,7 @@ const Navigate = () => {
     <div className="flex flex-col md:flex-row p-5 mb-12 gap-6 items-center justify-center ">
       <div className="flex gap-2">
         <Link to="/">
-          <div className="rounded-3xl shadow-lg shadow-[#6057ca] hover:scale-105 duration-200  px-6 p-3 cursor-pointer ">
+          <div className="rounded-3xl shadow-lg shadow-[#ffcdc2] hover:scale-105 duration-200  px-6 p-3 cursor-pointer ">
             <div className="flex gap-4">
               <img
                 src="/vape2.png"
@@ -20,7 +20,7 @@ const Navigate = () => {
           </div>
         </Link>
         <Link to="/product2">
-          <div className="   rounded-3xl shadow-lg shadow-[#6057ca] hover:scale-105 duration-200  px-6 p-3 cursor-pointer ">
+          <div className="   rounded-3xl shadow-lg shadow-[#ffcdc2] hover:scale-105 duration-200  px-6 p-3 cursor-pointer ">
             <img
               src="/vape3.png"
               alt="contact us"
@@ -30,7 +30,7 @@ const Navigate = () => {
         </Link>
 
         <Link to="/product3">
-          <div className="rounded-3xl shadow-lg shadow-[#6057ca] hover:scale-105 duration-200  px-6 p-3 cursor-pointer  ">
+          <div className="rounded-3xl shadow-lg shadow-[#ffcdc2] hover:scale-105 duration-200  px-6 p-3 cursor-pointer  ">
             <img
               src="/vape4.png"
               alt="contact us"
