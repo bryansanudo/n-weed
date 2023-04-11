1e1a074dfe0912356450e83d70079410c1390ed3
Bryan <bryansanudo95@gmail.com>
2023-04-11 14:48:52 -0500

    video

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { MdNightsStay, MdWbSunny, MdOutlineClose } from "react-icons/md";
-import { BiCart } from "react-icons/bi";
+import React from "react";
+import { MdNightsStay, MdWbSunny } from "react-icons/md";
+
 import logo from "@/assets/Logo.png";
 import logoPhoto from "@/assets/LogoPhoto.png";
+import logoVideo from "@/assets/LogoVideo.png";
 
 const Header = ({
   allProducts,
@@ -14,7 +15,7 @@ const Header = ({
   darkMode,
   setDarkMode,
 }) => {
-  const [active, setActive] = useState(false);
+  /*  const [active, setActive] = useState(false);
   const onDeleteProduct = (product) => {
     const results = allProducts.filter((item) => item.id !== product.id);
 
@@ -27,23 +28,30 @@ const Header = ({
     setAllProducts([]);
     setTotal(0);
     setCountProducts(0);
-  };
+  }; */
   return (
     <header className="fixed w-full bg-gray-900 text-white z-50">
       <nav className="flex justify-between items-center p-5">
-        <div className="flex gap-6">
+        <div className="flex md:gap-6 gap-2">
           <a href="https://eliana-nweed.netlify.app/">
             <img
               src={logo}
               alt=""
-              className="rounded-full object-cover hover:scale-105 transition-300  h-24 w-24  duration-300 shadow-lg   shadow-[#ffcdc2]"
+              className="rounded-full object-cover hover:scale-105 transition-300  md:h-24 md:w-24 h-20 w-20  duration-300 shadow-lg   shadow-[#ffcdc2]"
             />
           </a>
           <a href="https://eliana-nweed-gallery.netlify.app/">
             <img
               src={logoPhoto}
               alt=""
-              className="rounded-full object-cover hover:scale-105 transition-300  h-24 w-24  duration-300 shadow-lg   shadow-[#ffcdc2]"
+              className="rounded-full object-cover hover:scale-105 transition-300  md:h-24 md:w-24 h-20 w-20  duration-300 shadow-lg   shadow-[#ffcdc2]"
+            />
+          </a>
+          <a href="https://eliana-nweed-video.netlify.app/">
+            <img
+              src={logoVideo}
+              alt=""
+              className="rounded-full object-cover hover:scale-105 transition-300  md:h-24 md:w-24 h-20 w-20  duration-300 shadow-lg  shadow-[#ffcdc2]"
             />
           </a>
         </div>
@@ -52,7 +60,7 @@ const Header = ({
           {darkMode ? (
             <MdWbSunny className="w-8 h-8 cursor-pointer text-[#ffcdc2]" />
           ) : (
-            <MdNightsStay className=" w-8 h-8 cursor-pointer text-[#ffcdc2]" />
+            <MdNightsStay className="w-8 h-8 cursor-pointer text-[#ffcdc2]" />
           )}
         </div>
       </nav>
